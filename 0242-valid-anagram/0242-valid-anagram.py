3e046a47fb6d30f63f340f42f1b814e686f912a8
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-13 07:37:18 -0800

    Time: 54 ms (32.1%), Space: 16.9 MB (64.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -9,11 +9,9 @@ class Solution:
         for c in s:
             hashmap_s[c] += 1
 
-        # check if t char present in hashmap_s
         for char in t:
             if hashmap_s[char] == 0:
                 return False
-                
             hashmap_s[char] -= 1
 
         return True

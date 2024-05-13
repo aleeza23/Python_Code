a77c91a551f86db9fa027f2f04d7f78f09102d6b
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-13 07:35:33 -0800

    Time: 50 ms (53.28%), Space: 17.1 MB (28.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        # with default value as 0
         hashmap_s = collections.defaultdict(int)
-
-        # base case
+      
         if len(s) != len(t):
             return False
 
@@ -15,7 +13,7 @@ class Solution:
         for char in t:
             if hashmap_s[char] == 0:
                 return False
-
+                
             hashmap_s[char] -= 1
 
         return True

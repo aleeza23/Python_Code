0d1ebd7c7735f6929565e942bebdd62d57ee55e7
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-13 21:26:56 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,29 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        # brute force approach
-        ans = []
-        for i in range(len(nums)):
-            for j in range((i + 1), len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    ans.append(i)
-                    ans.append(j)
+        # brute force approach O(n^2) TC
+        # ans = []
+        # for i in range(len(nums)):
+        #     for j in range((i + 1), len(nums)):
+        #         if (nums[i] + nums[j]) == target:
+        #             ans.append(i)
+        #             ans.append(j)
                     
-        return ans            
+        # return ans
+
+
+
+        # another approach
+        hashmap = {}
+        ans = []
+
+        for i in range(len(nums) -1):
+            hashmap[(i, i + 1)] = nums[i] + nums[i + 1]
+            if hashmap[(i, i+1)] == target:
+                ans.append(i)
+                ans.append(i + 1)
+        return ans         
+
                
 
         

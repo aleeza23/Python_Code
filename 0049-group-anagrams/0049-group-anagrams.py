d36f61c09753c8489f27dc2258a2787045cb13b7
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-16 07:49:57 -0800

    Time: 91 ms (51.87%), Space: 22.5 MB (5.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,14 +5,9 @@ class Solution:
         
         for str in strs:
             array = [0]*26
-            
+
             for c in str:
                 array[ord(c) - ord("a")] += 1
             lookup[tuple(array)].append(str)
               
-        return lookup.values()       
-
-
-    
-        
-        
+        return lookup.values()        

b0d77015b252a19a34845c2441c5d557fc1032d3
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-18 07:33:09 -0800

    Time: 93 ms (50.55%), Space: 16.6 MB (63.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,15 +6,19 @@ class Solution:
 
         for r in range(len(board)):
             for c in range(len(board)):
-              
-                if board[r][c] == ".":
-                    continue 
 
-                if board[r][c] in ROWS[r] or board[r][c] in COLS[c] or board[r][c] in SUB_BOXES[r // 3, c // 3]:                    return False
+                if board[r][c] == ".":
+                    continue
+
+                if (
+                    board[r][c] in ROWS[r]
+                    or board[r][c] in COLS[c]
+                    or board[r][c] in SUB_BOXES[r // 3, c // 3]
+                ):
+                    return False
 
                 COLS[c].add(board[r][c])
                 ROWS[r].add(board[r][c])
                 SUB_BOXES[r // 3, c // 3].add(board[r][c])
-                
 
         return True

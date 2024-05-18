289a549f7d947d552632d2b8f640a012d50db9b5
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-17 22:08:15 -0800

    Time: 103 ms (10.05%), Space: 16.6 MB (63.53%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        COLS = collections.defaultdict(set)
+        ROWS = collections.defaultdict(set)
+        SUB_BOXES = collections.defaultdict(set)
+
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == ".":
+                    continue 
+
+                if board[r][c] in ROWS[r] or board[r][c] in COLS[c] or board[r][c] in SUB_BOXES[r // 3, c // 3]:                    return False
+
+                COLS[c].add(board[r][c])
+                ROWS[r].add(board[r][c])
+                SUB_BOXES[r // 3, c // 3].add(board[r][c])
+                print(ROWS)
+
+        return True

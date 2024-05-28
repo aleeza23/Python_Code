669589796817b0993022d38171afa9893dd94758
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-28 10:24:29 +0500

    Time: 41 ms (15.34%), Space: 16.7 MB (9.85%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -6,13 +6,11 @@ class Solution:
 
       
         for i in range(h_length - n_length + 1):
-            h_pointer = i
             n_pointer = 0          
 
-            while n_pointer < n_length and haystack[h_pointer] == needle[n_pointer]:
-                h_pointer += 1
+            while n_pointer < n_length and haystack[i + n_pointer] == needle[n_pointer]:
                 n_pointer += 1
-                print(h_pointer, n_pointer)                
+                print( n_pointer)                
 
             if n_pointer == n_length:
                 return i

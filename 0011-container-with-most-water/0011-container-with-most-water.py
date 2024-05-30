e2af2e3670ab713edb77da68eb2045643d1d6626
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-05-30 23:50:27 +0500

    Time: 532 ms (33.16%), Space: 28.9 MB (98.16%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,18 +4,65 @@ class Solution:
         # formula of area
         # area = width * height
 
+        # left_p = 0
+        # right_p = len(height) - 1
+        # res = 0
+
+        # while left_p < right_p:
+        #     area = (right_p - left_p) * min(height[left_p], height[right_p])
+        #     print(area)
+        #     res = max(res, area)
+
+        #     if height[left_p] < height[right_p]:
+        #         left_p += 1               
+        #     else:
+        #         right_p -= 1              
+
+        # return res
+
+
         left_p = 0
         right_p = len(height) - 1
-        res = 0
+        ans = 0
 
         while left_p < right_p:
-            area = (right_p - left_p) * min(height[left_p], height[right_p])
-            res = max(res, area)
+            w = right_p - left_p
+            h = min(height[left_p], height[right_p])
+            area = w * h
+            ans = max(ans , area)
 
             if height[left_p] < height[right_p]:
-                left_p += 1               
+                left_p += 1
             else:
-                right_p -= 1              
+                right_p -= 1
 
-        return res                 
+        return ans           
+
+
+
+
+
+
+
+
+
+
+
+
      
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

416de057ffbbf5e58caf209efe9f8beb0b6de39d
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-06-11 16:25:53 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,40 +2,39 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         dect_s1 = collections.defaultdict(int)
         dect_s2 = collections.defaultdict(int)
-        left_p = 0
-        right_p = 0
+
+        window_size = len(s1)
 
         if len(s1) > len(s2):
             return False
+
         # updated dict with s1 char
         for c in s1:
             dect_s1[c] += 1      
 
-        # updated 1st window in dict for s2
-        while right_p < len(s1):
-            dect_s2[s2[right_p]] += 1
-            right_p += 1
-            
-        print(right_p)
+        # compute the 1st window
+        for i in range(window_size):
+            dect_s2[s2[i]] += 1            
 
         # iterate on s2 
         for j in range(len(s2) - len(s1) + 1):
-            window = s2[j : j + len(s1)]
-            print(window)
+            # window = s2[j : j + len(s1)]
+            # print(window)
 
            
             if dect_s1 == dect_s2:
                 return True
 
           
-            if right_p < len(s2):
-                dect_s2[s2[right_p]] += 1
-                right_p += 1
+            if j + window_size < len(s2) - len(s1) + 1:
+                dect_s2[s2[j + window_size]] += 1
+            
 
            
-            if dect_s2[window[0]] == 1:
-                del dect_s2[window[0]]
-           
+            if dect_s2[s2[j]] == 1:
+                del dect_s2[s2[j]]
+            else:
+                dect_s2[s2[j]] -= 1
 
         return False
            
